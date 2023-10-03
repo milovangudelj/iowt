2f52f878cd1b759c6d4cbe4c94d1647de975eaab
milovangudelj <milovan.gudelj@gmail.com>
2023-10-03 14:07:02 +0200

    meh...

--- packages/ui/src/components/nav-user.tsx
+++ packages/ui/src/components/nav-user.tsx
@@ -9,33 +9,34 @@ export function NavUser() {
   const { signOut } = useAuth();
   const { session, isLoaded, isSignedIn } = useSession();
 
-  if (!isLoaded) {
-    return <p>Loading...</p>;
-  }
-  if (!isSignedIn) {
+  if (!isSignedIn && isLoaded) {
     redirect("/signin");
   }
 
   const router = useRouter();
-  const { user } = session;
+  const user = session?.user;
 
   return (
     <div className="ui-flex ui-items-center ui-gap-2">
       <div className="ui-w-8 ui-h-8 ui-rounded-full ui-overflow-hidden ui-border-white/[0.06] ui-bg-gray-900">
-        {/* <Image
-          src={user.imageUrl}
-          alt={`La tua immagine del profilo`}
-          width={32}
-          height={32}
-          className="ui-w-full ui-h-full ui-object-cover"
-        /> */}
+        {user && (
+          <Image
+            src={user.imageUrl}
+            alt={`La tua immagine del profilo`}
+            width={32}
+            height={32}
+            className="ui-w-full ui-h-full ui-object-cover"
+          />
+        )}
       </div>
-      <div className="ui-flex ui-flex-col">
-        <span>
-          {user.firstName} {user.lastName}
-        </span>
-        <span>{user.primaryEmailAddress?.emailAddress}</span>
-      </div>
+      {user && (
+        <div className="ui-flex ui-flex-col">
+          <span>
+            {user.firstName} {user.lastName}
+          </span>
+          <span>{user.primaryEmailAddress?.emailAddress}</span>
+        </div>
+      )}
       <button
         onClick={() => {
           signOut();
