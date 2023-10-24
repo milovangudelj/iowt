7c9174e801528310afa0d71a147fc6fb1f2fade5
milovangudelj <milovan.gudelj@gmail.com>
2023-10-24 23:39:51 +0200

    Removed metadata and converted component to client

--- apps/dashboard/app/(auth)/signin/page.tsx
+++ apps/dashboard/app/(auth)/signin/page.tsx
@@ -1,11 +1,7 @@
-import { Metadata } from "next";
+"use client";
+
 import { AuthForm } from "ui";
 
-export const metadata: Metadata = {
-  title: "IOWT - Sign in",
-  description: "Sign in to access the IOWT Dashboard",
-};
-
-export default async function Page() {
+export default function Page() {
   return <AuthForm intent="signin" />;
 }
